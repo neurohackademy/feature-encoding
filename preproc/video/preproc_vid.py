acadc1b8c56d4b5c3c143634052bab7ca314eaa8
jungheejung <spacetop@453A-PP.localdomain>
2021-10-26 19:57:05 -0400

    convert algonaut videos to frames

---
+++ preproc/video/preproc_vid.py
@@ -0,0 +1,54 @@
+# %% 
+"""
+convert videos to frames
+also rename videos from original algonaut folders to video_000001.mp4
+"""
+# TODO: 
+# [x] 1. grab list of folders from INDIR, 
+# [x] 2. make new dir in OUTDIR resampled
+# [x] 3. change folder name to indices instead of original gibberish folder name. 
+
+# %% libraries
+import os, glob, re, shutil
+from pathlib import Path
+import ffmpeg
+
+def get_new_videoname(videoname):
+	name, type = videoname.split(".")
+	name = name + "_256x256"
+	new_videoname = name + '.' + type
+	return new_videoname
+
+# %%
+main_dir = '/mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw'
+in_dir = os.path.join(main_dir, 'training')
+out_dir = os.path.join(main_dir,'resampled', 'training')
+
+action_list = glob.glob(os.path.join(in_dir,'*')) # asking, applauding
+
+for action_dir in action_list:
+    print("action dir: ", action_dir)
+    action_name = os.path.basename(action_dir)
+    video_list_tmp = glob.glob(os.path.join(action_dir,'*'))
+
+    for index, old_videoname in enumerate(video_list_tmp):
+        print("old_videoname: ", old_videoname)
+        extension = os.path.basename(old_videoname).split('.')[-1]
+        print("extension", extension)
+        if extension == 'mp4':
+            new_path = os.path.join(out_dir, action_name)
+            Path(new_path).mkdir(parents = True, exist_ok = True)
+            print("newpath: ", new_path)
+            new_filename = os.path.join(new_path, f'video_{index:06d}.mp4')
+
+            ## ffmpeg stuff
+            stream = (ffmpeg.input(old_videoname).filter('scale', width = 365, height = 258).filter('crop', 256, 256).output(get_new_videoname(new_filename)))
+            ffmpeg.run(stream)
+
+            # TODO Nov 02:
+            # ffmpeg split into frames
+            # here's the code in bash
+            # ffmpeg -i ${OUTDIR}/${FILENAME}/${FILENAME}_256x256.mp4 -r 24 ${OUTDIR}/${FILENAME}/frames/frame%06d.jpg -hide_banner
+            break
+    break
+
--- preproc/video/preproc_vid.sh
+++ preproc/video/preproc_vid.sh
@@ -1,18 +1,31 @@
-# old version > output 224 x 224
-INDIR=/home/ubuntu/data/raw
-OUTDIR=/home/ubuntu/data/resampled
-FILENAME="7T_MOVIE4_HO2_v2"
-ffmpeg -i ${INDIR}/${FILENAME}.mp4 -filter:v "scale=320:225, crop=224:224" -c:a copy ${OUTDIR}/${FILENAME}_224x224.mp4
-
+#!/bin/bash
+# # old version > output 224 x 224
+# INDIR=/home/ubuntu/data/raw
+# OUTDIR=/mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw/resampled
+# FILENAME="7T_MOVIE4_HO2_v2"
+# ffmpeg -i ${INDIR}/${FILENAME}.mp4 -filter:v "scale=320:225, crop=224:224" -c:a copy ${OUTDIR}/${FILENAME}_224x224.mp4
 # new version > output 256 x 256
-INDIR=/home/ubuntu/data/raw
-OUTDIR=/home/ubuntu/data/resampled
-FILENAME="7T_MOVIE4_HO2_v2"
-ffmpeg -i ${INDIR}/${FILENAME}.mp4 -filter:v "scale=365:258, crop=256:256" -c:a copy ${OUTDIR}/${FILENAME}_256x256.mp4
+# _______________________
+INDIR="/mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw/training/applauding"
+OUTDIR="/mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw/resampled/training/applauding"
 
-# converting video to jpeg
-INDIR=/home/ubuntu/data/raw
-OUTDIR=/home/ubuntu/data/resampled
-FILENAME="7T_MOVIE1_CC1_v2_256x256"
+for FULLFILE in $(ls ${INDIR})
+do
 mkdir -p ${OUTDIR}/${FILENAME}
-ffmpeg -i ${INDIR}/${FILENAME}.mp4 -r 24 ${OUTDIR}/${FILENAME}/frame%06d.jpg -hide_banner
+FILENAME="${FULLFILE%.*}"
+ffmpeg -i ${INDIR}/${FILENAME}.mp4 -filter:v "scale=365:258, crop=256:256" -c:a copy ${OUTDIR}/${FILENAME}/${FILENAME}_256x256.mp4
+done
+# _______________________
+
+# /mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw/resampled/training/applauding/D9draVjaGQI_5/D9draVjaGQI_5_256x256
+# converting video to jpeg
+# INDIR=/mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw/training/applauding
+# OUTDIR=/mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw/resampled/training/applauding
+
+# for FULLFILE in $(ls ${OUTDIR})
+# do
+# # ..\Moments_in_Time_Raw\resampled\training\applauding\D9draVjaGQI_5\frames
+# FILENAME="${FULLFILE%.*}"
+# mkdir -p ${OUTDIR}/${FILENAME}/frames
+# ffmpeg -i ${OUTDIR}/${FILENAME}/${FILENAME}_256x256.mp4 -r 24 ${OUTDIR}/${FILENAME}/frames/frame%06d.jpg -hide_banner
+# done
