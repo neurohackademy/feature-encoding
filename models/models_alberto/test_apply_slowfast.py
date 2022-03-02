3fa3155da0d5be4c187c185ebeafa2e0387a432a
jungheejung <heejung.jung@colorado.edu>
2022-03-01 19:54:06 -0500

    update file path and library

--- models/models_alberto/test_apply_slowfast.py
+++ models/models_alberto/test_apply_slowfast.py
@@ -9,14 +9,15 @@ import models as algo_models
 #     print(activation, activations[activation].shape)
 
 # %% 
-# vid
-# 
-# eo_path = 'C:\Users\Spacetop\Documents\Moments_in_Time_Raw_v2\Moments_in_Time_Raw\resampled\training\applauding'
+# video_path = 'C:\Users\Spacetop\Documents\Moments_in_Time_Raw_v2\Moments_in_Time_Raw\resampled\training\applauding'
 video_path = '/mnt/c/Users/Spacetop/Documents/Moments_in_Time_Raw_v2/Moments_in_Time_Raw/resampled/training/ascending'
 
 model_name = 'slowfast'
 # TODO: need to train model on actual algonauts videos
 # currently, we're only grabbing pre-trained weights in this code.
+# * dataloader
+# https://pytorch.org/hub/facebookresearch_pytorchvideo_slowfast/
+# https://github.com/facebookresearch/SlowFast/blob/main/slowfast/datasets/loader.py
 wrapped_model = algo_models.select_model(model_name)()
 activations = algo_models.get_activations(wrapped_model, video_path, True)
 print(activations)
